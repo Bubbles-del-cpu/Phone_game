64f31b1a15b1d4736092b2449cb7d91a9e0fd855
johnfirmin <johnfirmin@gmail.com>
2025-09-15 17:07:01 +0100

    Batched Notifications
    + Added batching logic and settings to DialogueUIManager. Notifications
    from the same character are grouped so only one is shown at a time,
    with an option to update the text to the latest message if desired. A
    flag can also restrict the screen to a single notification, replacing
    the previous one regardless of display time. Note: this system augments
    batching and only applies when a new character sends a message.

--- Assets/Scripts/UI/Notification.cs
+++ Assets/Scripts/UI/Notification.cs
@@ -7,34 +7,60 @@ public class Notification : MonoBehaviour
 {
     [SerializeField] ProfileIcon icon;
     [SerializeField] TMP_Text nameLabel;
-    [SerializeField] TMP_Text label;
+    [SerializeField] TMP_Text _label;
     [SerializeField] Button closeButton;
     [SerializeField] Image[] typeIcons;
     [SerializeField] float displayTime = 5f;
 
-    NotificationType type;
-    Button button;
-    DialogueCharacterSO character;
+    private NotificationType _type;
+    private Button _button;
+    private DialogueCharacterSO _character;
     private CanvasGroup _cg;
+    public string Label => _label.text;
+
+    private float _destroyTimer = 0;
 
     private void Start()
     {
-        button = GetComponent<Button>();
-        button.onClick.AddListener(() => OnClicked());
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(() => OnClicked());
         closeButton.onClick.AddListener(() => Destroy(gameObject));
 
         _cg = GetComponent<CanvasGroup>();
         _cg.alpha = 1;
-        Destroy(gameObject, DialogueUIManager.Instance.NotificationDisplayLength);
+    }
+
+    public void Setup(NotificationType type, DialogueCharacterSO character, string message)
+    {
+        switch (type)
+        {
+            case NotificationType.SocialMedia:
+                _label.text = "has made a new post";
+                break;
+            case NotificationType.Message:
+                _label.text = message;
+                break;
+        }
+
+        Type = type;
+        Character = character;
+        _destroyTimer = 0;
+    }
+
+    private void Update()
+    {
+        _destroyTimer += Time.deltaTime;
+        if (_destroyTimer >= DialogueUIManager.Instance.NotificationDisplayLength)
+            Destroy(gameObject);
     }
 
     void OnClicked()
     {
-        switch(type)
+        switch(_type)
         {
             default:
                 //GameManager.Instance.SocialMediaCanvas.Close();
-                GameManager.Instance.MessagingCanvas.Open(character, fromNotification: true);
+                GameManager.Instance.MessagingCanvas.Open(_character, fromNotification: true);
                 break;
             case NotificationType.SocialMedia:
                 //GameManager.Instance.MessagingCanvas.Close();
@@ -47,35 +73,24 @@ public class Notification : MonoBehaviour
 
     public NotificationType Type
     {
-        get { return type; }
+        get { return _type; }
         set
         {
-            type = value;
+            _type = value;
             typeIcons[(int)value].enabled = true;
-
-            switch(type)
-            {
-                default:  // message
-                    label.text = "sent you a new message";
-                    break;
-                case NotificationType.SocialMedia:
-                    label.text = "shared a new post";
-                    break;
-            }
         }
     }
 
     public DialogueCharacterSO Character
     {
+        get => _character;
         set
         {
-            character = value;
+            _character = value;
             icon.Character = value;
             nameLabel.text = value.name;
         }
     }
-
-    public string Label { set { label.text = value; } }
 
     public enum NotificationType
     {
