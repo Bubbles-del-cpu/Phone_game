f8d410ada45b8149d82f00257202e10a30c1b3b5
johnfirmin <johnfirmin@gmail.com>
2025-10-15 19:23:48 +0100

    SocialMediaPostSO Localization
    + Added localization support for SocialMediaPostSO and SocialMediaPost
      components.
    
    * Updated existing social media posts to have existing message text
      placed into localization tables.

--- Assets/Scripts/Editor/SocialMediaPostSOEditor.cs
+++ Assets/Scripts/Editor/SocialMediaPostSOEditor.cs
@@ -1,15 +1,19 @@
+using System;
+using System.Collections.Generic;
+using MeetAndTalk;
 using UnityEditor;
 using UnityEditor.Rendering;
 
 [CustomEditor(typeof(SocialMediaPostSO))]
 public class SocialMediaPostSOEditor : UnityEditor.Editor
 {
-    SerializedProperty _characterProp, _messageProp, _typeProp, _imageProp, _videoProp, _videoThumbnailProp, _commentsProp, _displayProp, _backgroundProp;
+    SerializedProperty _characterProp, _messageProp, _messageTextsProp, _typeProp, _imageProp, _videoProp, _videoThumbnailProp, _commentsProp, _displayProp, _backgroundProp;
 
     void OnEnable()
     {
         _characterProp = serializedObject.FindProperty("Character");
-        _messageProp = serializedObject.FindProperty("Message");
+        //_messageProp = serializedObject.FindProperty("Message");
+        _messageTextsProp = serializedObject.FindProperty("MessageTexts");
         _typeProp = serializedObject.FindProperty("MediaType");
         _commentsProp = serializedObject.FindProperty("Comments");
         _imageProp = serializedObject.FindProperty("Image");
@@ -18,11 +22,35 @@ public class SocialMediaPostSOEditor : UnityEditor.Editor
         _displayProp = serializedObject.FindProperty("GalleryVisibility");
         _backgroundProp = serializedObject.FindProperty("NotBackgroundCapable");
     }
+
+    private void CorrectForLocalization()
+    {
+        var post = (SocialMediaPostSO)target;
+        if (post.Message != "")
+        {
+            post.MessageTexts = new List<LanguageGeneric<string>>();
+
+            foreach (MeetAndTalk.Localization.LocalizationEnum item in Enum.GetValues(typeof(MeetAndTalk.Localization.LocalizationEnum)))
+            {
+                post.MessageTexts.Add(new LanguageGeneric<string>()
+                {
+                    languageEnum = item,
+                    LanguageGenericType = post.Message
+                });
+
+                post.Message = "";
+            }
+        }
+
+        EditorUtility.SetDirty(target);
+    }
     public override void OnInspectorGUI()
     {
+        CorrectForLocalization();
         serializedObject.Update();
         EditorGUILayout.PropertyField(_characterProp);
-        EditorGUILayout.PropertyField(_messageProp);
+        //EditorGUILayout.PropertyField(_messageProp);
+        EditorGUILayout.PropertyField(_messageTextsProp);
         EditorGUILayout.PropertyField(_typeProp);
 
         switch ((MediaType)_typeProp.enumValueIndex)
